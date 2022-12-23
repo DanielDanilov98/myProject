3264b8447ac12529dd090ce9e8a377e35bc5475b
Daniel Danilov <danieldanilov1998@gmail.com>
2022-12-23 21:34:07 +0200

    g

---
+++ myProject/src/models/pageModel.js
@@ -0,0 +1,10 @@
+const PAGES = {
+  HOME: "home-page",
+  NEWS: "news-page",
+  CATEGORY: "category-page",
+  ABOUT: "about-page",
+  CONTACT: "contact-page",
+  LOGIN: "login-page",
+  ERROR404: "404-error-page",
+};
+export default PAGES;
