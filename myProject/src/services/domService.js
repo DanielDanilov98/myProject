60775a985350d440ff0dd78bd3a25153bb3cf45d
Daniel Danilov <danieldanilov1998@gmail.com>
2023-01-24 14:12:08 +0200

    home page

---
+++ myProject/src/services/domService.js
@@ -0,0 +1,14 @@
+//#region nav links
+export const HOME_PAGE_LINK = document.querySelector("#home-link");
+export const ABOUT_PAGE_LINK = document.querySelector("#about-page-link");
+
+export const CREATE_PRODUCT_PAGE_LINK = document.querySelector("#create-product-page-link");
+export const LOGIN_PAGE_LINK = document.querySelector("#login-page-link");
+export const SIGNUP_PAGE_LINK = document.querySelector("#signup-page-link");
+
+export const ADD_PRODUCT_LINK_CONTAINER = document.querySelector("#add-product-link-container");
+export const LOGIN_LINK_CONTAINER = document.querySelector("#login-link-container");
+export const LOGOUT_LINK_CONTAINER = document.querySelector("#logout-link-container");
+export const EDIT_USER_PAGE_LINK = document.querySelector("#edit-user-link");
+export const LOGOUT_LINK = document.querySelector("#logout-link");
+//#endregion
